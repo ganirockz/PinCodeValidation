eb2df42ea35fae160be95bc8b4ba2b8e953b96b2
ganirockz <ganeshrapeti8@gmail.com>
2020-09-28 15:44:27 +0530

    [Ganesh]Added: Regex pattern to validate pincode 400088

---
+++ src/com/capgemini/PinCodeValidation.java
@@ -0,0 +1,33 @@
+package com.capgemini;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinCodeValidation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Pattern pattern = Pattern.compile("[0-9]{6}");
+		while (true) {
+			System.out.println("Do you want to enter Pincode: \nselect \n1.yes\n2.no");
+			int option = Integer.parseInt(sc.nextLine());
+			if(option == 1) {
+			System.out.println("Enter the Pincode to check pattern :");
+			String input = sc.nextLine();
+			Matcher m = pattern.matcher(input);
+			if (m.find()) {
+				System.out.println("Valid");
+			} else {
+				System.out.println("Not Valid");
+			}
+			}
+			else{
+				System.out.println("Thank you!");
+				break;
+			}
+		}
+		sc.close();
+	}
+
+}
